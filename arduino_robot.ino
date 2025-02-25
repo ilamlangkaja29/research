6462fbba3984ef11c9fe0cae343c7c087ae36e0f
Surely Win Dilag <108412632+Win1817@users.noreply.github.com>
2025-02-25 20:39:37 +0800

    Update arduino_robot.ino
    To test

--- arduino_robot.ino
+++ arduino_robot.ino
@@ -1,122 +1,116 @@
 #include <Servo.h>
+#include <NewPing.h>
+#include <HX711.h>
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
-#include "HX711.h"
 
-// Motor Driver Pins
+// Pin Configuration
+#define IR_SENSOR 4            // Line Following IR sensor
+#define TRIG_PIN A0            // Ultrasonic sensor trigger
+#define ECHO_PIN A0            // Ultrasonic sensor echo
+#define BIN_SENSOR 11          // E18-D80NK IR sensor for bin detection
+#define SERVO_PIN 12           // Servo motor for bin lid
+#define BUZZER A1              // Buzzer for alerts
+
+// Motor Driver (L298N)
 #define ENA 5
 #define IN1 6
 #define IN2 7
-#define ENB 10
 #define IN3 8
 #define IN4 9
+#define ENB 10
 
-// Bluetooth Module
-#define BT_RX 2  // SoftwareSerial (if needed)
-#define BT_TX 3
-
-// IR Sensors
-#define IR_MOTOR 4
-#define IR_BIN 11
-
-// Servo Motor
-Servo binServo;
-#define SERVO_PIN 12
-
-// Ultrasonic Sensor
-#define TRIG 13
-#define ECHO A0
-
-// Buzzer
-#define BUZZER A1
+// Bluetooth module (HC-05)
+#define BT_RX 0
+#define BT_TX 1
 
 // Load Cell (HX711)
-#define LOADCELL_DOUT_PIN A2
-#define LOADCELL_SCK_PIN A3
-HX711 scale;
+#define LOADCELL_DOUT 2
+#define LOADCELL_SCK 3
 
 // LCD Display
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
+// Sensors and Components
+NewPing sonar(TRIG_PIN, ECHO_PIN, 200);  // Ultrasonic sensor
+Servo binServo;
+HX711 scale;
+
+// Variables
+char command;   // Bluetooth command
+long distance;  // Ultrasonic distance
+
 void setup() {
-    Serial.begin(9600);
-    pinMode(ENA, OUTPUT); pinMode(IN1, OUTPUT); pinMode(IN2, OUTPUT);
-    pinMode(ENB, OUTPUT); pinMode(IN3, OUTPUT); pinMode(IN4, OUTPUT);
-    pinMode(IR_MOTOR, INPUT); pinMode(IR_BIN, INPUT);
-    pinMode(TRIG, OUTPUT); pinMode(ECHO, INPUT);
+    Serial.begin(9600); // Bluetooth communication
+    
+    // Motor Pins
+    pinMode(ENA, OUTPUT);
+    pinMode(IN1, OUTPUT);
+    pinMode(IN2, OUTPUT);
+    pinMode(IN3, OUTPUT);
+    pinMode(IN4, OUTPUT);
+    pinMode(ENB, OUTPUT);
+
+    // Sensor Pins
+    pinMode(IR_SENSOR, INPUT);
+    pinMode(BIN_SENSOR, INPUT);
     pinMode(BUZZER, OUTPUT);
-    
+
+    // Load Cell
+    scale.begin(LOADCELL_DOUT, LOADCELL_SCK);
+
+    // Servo
     binServo.attach(SERVO_PIN);
-    binServo.write(0);
-    
-    scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
-    lcd.init();
+    binServo.write(0); // Initial position
+
+    // LCD Display
+    lcd.begin();
     lcd.backlight();
     lcd.setCursor(0, 0);
-    lcd.print("Weight: 0g");
+    lcd.print("Robot Initialized");
 }
 
 void loop() {
-    bluetoothControl();
-    IR_SensorControl();
-    ultrasonicAvoidance();
-    readWeight();
-}
-
-// Bluetooth Control for Motors
-void bluetoothControl() {
-    if (Serial.available()) {
-        char command = Serial.read();
-        if (command == 'F') moveForward();
-        else if (command == 'B') moveBackward();
-        else if (command == 'L') turnLeft();
-        else if (command == 'R') turnRight();
-        else if (command == 'S') stopMotors();
-    }
-}
-
-// IR Sensor Control
-void IR_SensorControl() {
-    if (digitalRead(IR_MOTOR) == LOW) stopMotors();
-    if (digitalRead(IR_BIN) == LOW) openBinCover();
-}
-
-// Open Bin Cover
-void openBinCover() {
-    binServo.write(90);
-    delay(3000);
-    binServo.write(0);
-}
-
-// Ultrasonic Sensor Avoidance
-void ultrasonicAvoidance() {
-    long duration;
-    digitalWrite(TRIG, LOW);
-    delayMicroseconds(2);
-    digitalWrite(TRIG, HIGH);
-    delayMicroseconds(10);
-    digitalWrite(TRIG, LOW);
-    duration = pulseIn(ECHO, HIGH);
-    int distance = duration * 0.034 / 2;
-    if (distance < 15) {
+    // Check Ultrasonic Distance
+    distance = sonar.ping_cm();
+    if (distance > 0 && distance < 10) {
         stopMotors();
         digitalWrite(BUZZER, HIGH);
         delay(1000);
         digitalWrite(BUZZER, LOW);
     }
-}
 
-// Read Weight from Load Cell
-void readWeight() {
-    float weight = scale.get_units();
-    lcd.clear();
-    lcd.setCursor(0, 0);
+    // Line Following (Automatic Mode)
+    if (digitalRead(IR_SENSOR) == LOW) {
+        moveForward();
+    } else {
+        stopMotors();
+    }
+
+    // Bin Detection & Servo Control
+    if (digitalRead(BIN_SENSOR) == LOW) {
+        binServo.write(90); // Open lid
+        delay(2000);
+        binServo.write(0);  // Close lid
+    }
+
+    // Bluetooth Control
+    if (Serial.available()) {
+        command = Serial.read();
+        executeBluetoothCommand(command);
+    }
+
+    // Load Cell Measurement
+    float weight = scale.get_units(10);
+    lcd.setCursor(0, 1);
     lcd.print("Weight: ");
-    lcd.print(weight, 1);
+    lcd.print(weight);
     lcd.print("g");
+
+    delay(100);
 }
 
-// Motor Functions
+// Motor Control Functions
 void moveForward() {
     digitalWrite(IN1, HIGH);
     digitalWrite(IN2, LOW);
@@ -140,6 +134,8 @@ void turnLeft() {
     digitalWrite(IN2, HIGH);
     digitalWrite(IN3, HIGH);
     digitalWrite(IN4, LOW);
+    analogWrite(ENA, 150);
+    analogWrite(ENB, 150);
 }
 
 void turnRight() {
@@ -147,6 +143,8 @@ void turnRight() {
     digitalWrite(IN2, LOW);
     digitalWrite(IN3, LOW);
     digitalWrite(IN4, HIGH);
+    analogWrite(ENA, 150);
+    analogWrite(ENB, 150);
 }
 
 void stopMotors() {
@@ -154,4 +152,16 @@ void stopMotors() {
     digitalWrite(IN2, LOW);
     digitalWrite(IN3, LOW);
     digitalWrite(IN4, LOW);
+}
+
+// Bluetooth Command Execution
+void executeBluetoothCommand(char cmd) {
+    switch (cmd) {
+        case 'F': moveForward(); break;
+        case 'B': moveBackward(); break;
+        case 'L': turnLeft(); break;
+        case 'R': turnRight(); break;
+        case 'S': stopMotors(); break;
+        default: break;
+    }
 }
