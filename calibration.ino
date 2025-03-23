72f1fd6d01e3859556f44a77bd8ee0c1ffff31dc
Surely Win Dilag <108412632+Win1817@users.noreply.github.com>
2025-03-23 12:35:23 +0800

    Update calibration.ino

--- calibration.ino
+++ calibration.ino
@@ -1,29 +1,39 @@
 #include <HX711_ADC.h>
 
-#define LOADCELL_DOUT_PIN 6
-#define LOADCELL_SCK_PIN 7
+#define LOADCELL_DOUT_PIN 2  // Changed to pin 2
+#define LOADCELL_SCK_PIN 3   // Changed to pin 3
 
 HX711_ADC scale(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
+float fixed_weight = 0;  
 
 void setup() {
   Serial.begin(115200);
-  Serial.println("Starting HX711...");
+  Serial.println("Initializing HX711...");
 
   scale.begin();
-  delay(500);  // Allow HX711 to stabilize
+  delay(500);
+
+  Serial.println("Remove all weight and wait...");
+  delay(3000);
 
   Serial.println("Taring...");
   scale.tare();
   delay(2000);
 
-  Serial.println("Start measuring...");
+  Serial.println("Calibration complete.");
 }
 
 void loop() {
-  if (scale.update()) {
-    float raw_value = scale.getData();
-    Serial.print("Raw Sensor Value: ");
-    Serial.println(raw_value);
+  scale.update();  
+  float current_weight = scale.getData();
+
+  if (abs(current_weight - fixed_weight) > 1.0) {  
+    fixed_weight = current_weight;  
   }
+
+  Serial.print("Stable Weight: ");
+  Serial.print(fixed_weight);
+  Serial.println(" g");
+
   delay(500);
 }
